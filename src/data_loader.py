fe38af852301c185d694a19581681001bbee033d
Salay Bhatia <salay24@gmail.com>
2025-12-08 01:11:03 +0800

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,15 @@
+import yfinance as yf
+import pandas as pd
+
+def load_data(tickers, start_date, end_date=None):
+    data = yf.download(
+        tickers,
+        start=start_date,
+        end=end_date,
+        auto_adjust=False,
+        progress=False
+    )
+    
+    prices = data["Adj Close"]
+    return prices
 
